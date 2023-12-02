cbfc75caa9919b1052460da4bea26216ab1e2ffe
Konkitoman <konkitoman@users.noreply.github.com>
2023-12-02 15:04:01 +0200

    Cleanup

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,3 @@
-let sample_text = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-"
-
 type game't = {
   game: int;
   r: int;
@@ -73,10 +66,6 @@ let rec get_games = fun str l->
 
 let filter = fun games r g b -> List.filter (fun game -> game.r <= r && game.g <= g && game.b <= b ) games
 
-let print_games = fun games -> List.iter (
-  fun g -> Format.printf "Game %i: r: %i, g: %i, b: %i\n" g.game g.r g.g g.b
-  ) games
-
 let rec sum = fun list start fn -> match list
   with e :: res -> sum res (start + fn e) fn
   | [] -> start
@@ -86,7 +75,6 @@ let games_sum = fun games -> sum games 0 (fun g -> g.game)
 let run = fun games r g b -> 
   (Format.printf "Max r: %i, g: %i, b: %i\n" r g b);
   let games = filter games r g b in
-  print_games games;
   let sum = games_sum games in
   Format.printf "Sum: %i\n" sum
 
@@ -94,19 +82,16 @@ let rec read_all = fun file str -> try read_all file (input_char file :: str) wi
 
 let read_file = fun filename -> read_all (open_in filename) []
 
-let list_of_string = fun s -> List.init (String.length s) (String.get s)
-let string_of_list = fun l -> String.init (List.length l) (List.nth l)
-
-
 let file_data = read_file "input2.txt";;
 let _,games = get_games file_data [];;
 
 let () = run games 12 13 14
 
+
+
 let games_sum'5 = fun games -> sum games 0 (fun g -> g.r * g.g * g.b)
 
 let run'5 = fun games -> 
-  print_games games;
   let sum = games_sum'5 games in
   Format.printf "Sum'5: %i\n" sum
 
